dceab062283b296f962dbd89e99dc37bde5db651
Michal Cyprian <mcyprian@redhat.com>
2015-09-16 14:35:17 +0200

    Reduce set of dependancies to packages given as arguments

--- sclbuilder/bin.py
+++ sclbuilder/bin.py
@@ -6,28 +6,27 @@ from sclbuilder.graph import PackageGraph
 from sclbuilder.exceptions import UnknownRepoException
 
 
-CONTEXT_SETTINGS = dict(help_option_names = ['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('packages', nargs=-1)
 @click.option('-r',
-            help='Repo to search dependancies of the Packages (default: "{0}")'.format(
-            settings.DEFAULT_REPO),
-            default=settings.DEFAULT_REPO,
-            metavar='REPO')
+              help='Repo to search dependancies of the Packages (default: "{0}")'.format(
+              settings.DEFAULT_REPO),
+              default=settings.DEFAULT_REPO,
+              metavar='REPO')
 @click.option('--visual / --no-visual',
-             default=True,
-             help='Enable / disable visualization of relations between pacakges')
+              default=True,
+              help='Enable / disable visualization of relations between pacakges')
 
 def main(visual, r, packages):
-    Graph = PackageGraph(r)
+    pg = PackageGraph(set(packages), r)
     try:
-        for package in packages:
-            Graph.process_deps(package, False)
+        pg.make_graph()
     except UnknownRepoException:
         print('Repository {} is probably disabled'.format(r))
         sys.exit(1)
-    Graph.plan_building_order()
-    Graph.run_building()
+    pg.plan_building_order()
+    pg.run_building()
     if visual:
-        Graph.show_graph()
+        pg.show_graph()
--- sclbuilder/graph.py
+++ sclbuilder/graph.py
@@ -3,31 +3,41 @@ import matplotlib.pyplot as plt
 import locale
 from subprocess import Popen, PIPE
 
-import sclbuilder.settings
 from sclbuilder.exceptions import UnknownRepoException
 
 class PackageGraph(object):
-    def __init__(self, repo):
+    def __init__(self, packages, repo):
+        self.packages = packages
         self.repo = repo
-        self.G = nx.MultiDiGraph()
+        self.graph = nx.MultiDiGraph()
         self.processed_packages = set()
         self.built_packages = set()
+        self.num_of_deps = {}
+        self.circular_deps = set()
+
+    def make_graph(self):
+        '''
+        Process all the packages, finds their dependancies and makes graph of
+        relations
+        '''
+        for package in self.packages:
+            self.process_deps(package)
 
     def show_graph(self):
         '''
         Draws nodes, edges, labels and shows graph
         '''
-        try: 
-            pos = nx.graphviz_layout(self.G)
+        try:
+            pos = nx.graphviz_layout(self.graph)
         except (ImportError, AttributeError):
-            pos = nx.circular_layout(self.G)
-        nx.set_node_attributes(self.G, 'pos', pos)
-        nx.draw(self.G, pos, node_size=10000, with_labels=True, node_color="#CDE0F0")
+            pos = nx.circular_layout(self.graph)
+        nx.set_node_attributes(self.graph, 'pos', pos)
+        nx.draw(self.graph, pos, node_size=10000, with_labels=True, node_color="#CDE0F0")
         plt.show()
-     
-    def process_deps(self, package, recursive=True):
+
+    def process_deps(self, package, recursive=False):
         '''
-        Adds edge between package and each of its dependancies, if 
+        Adds edge between package and each of its dependancies,
         pacakge was not processes before. When recursive is True
         calls itself for each dependance.
         '''
@@ -35,59 +45,80 @@ class PackageGraph(object):
             return
         else:
             print(package)
-            self.G.add_node(package)
+            self.graph.add_node(package)
             self.processed_packages.add(package)
-            for dep in get_deps(package, self.repo):
-                self.G.add_edge(package, dep)
+            for dep in self.get_deps(package):
+                self.graph.add_edge(package, dep)
                 if recursive:
                     self.process_deps(dep)
 
     def plan_building_order(self):
         '''
         creates dictionary of packages , keys of the dictionaty are
-        numbers of dependancies, values are names of the packages, 
+        numbers of dependancies, values are names of the packages,
         packages with circular dependancies are stored in special set
         '''
-        self.num_of_deps = {}
-        self.circular_deps = set()
-        for node in self.G.nodes():
-            if set(self.G.successors(node)) & set(self.G.predecessors(node)):
+        for node in self.graph.nodes():
+            if set(self.graph.successors(node)) & set(self.graph.predecessors(node)):
                 self.circular_deps.add(node)
             else:
-                update_dict(self.num_of_deps, len(self.G.successors(node)), node)
+                update_dict(self.num_of_deps, len(self.graph.successors(node)), node)
 
         print("\nTo build:")
         for num in sorted(self.num_of_deps.keys()):
             print("deps {}   {}".format(num, self.num_of_deps[num]))
         print("circular dependancy: {}".format(self.circular_deps))
-    
+
     def run_building(self):
         '''
-        Simulate building of packages in right order, first builds all packages with no deps, 
+        Simulate building of packages in right order, first builds all packages
+        with no deps,
         than iterate over others and builds packages which have satisfied all their deps
         '''
         if not self.num_of_deps:
             print("Nothing to build")
             return
-        for package in self.num_of_deps[0]:      # First we build package with no deps
-            self.build(package)
+        
         all_packages = set()
-        for node in self.G.nodes():
+        for node in self.graph.nodes():
             all_packages.add(node)
+        
+        if 0 in self.num_of_deps.keys():
+            for package in self.num_of_deps[0]:      # First we build package with no deps
+                self.build(package)
 
-        packages_to_build =  all_packages - self.circular_deps
+        packages_to_build = all_packages - self.circular_deps
         while packages_to_build != self.built_packages:
-            for num in sorted(self.num_of_deps.keys())[1:]:
+            for num in sorted(self.num_of_deps.keys()):
+                if num == 0:
+                    continue
                 for package in self.num_of_deps[num]:
                     if package not in self.built_packages and self.deps_satisfied(package):
-                        self.build(package)
+                       self.build(package)
+
+    def get_deps(self, package):
+        '''
+        Returns all dependancies of the package found in selected repo
+        '''
+        proc = Popen(["dnf", "repoquery", "--arch=src", "--disablerepo=*",
+                      "--enablerepo=" + self.repo, "--requires", package], stdout=PIPE, stderr=PIPE)
+        stream_data = proc.communicate()
+        if proc.returncode:
+            if stream_data[1].decode(locale.getpreferredencoding()) ==\
+                "Error: Unknown repo: '{0}'\n".format(self.repo):
+                raise UnknownRepoException('Repository {} is probably disabled'.format(self.repo))
+        all_deps = set(stream_data[0].decode(locale.getpreferredencoding()).splitlines()[1:])
+        return all_deps & self.packages
+
+
 
     def deps_satisfied(self, package):
         '''
         Compares package deps with self.build_packages to
         check if are all dependancies already built
         '''
-        if (set(self.G.successors(package)) - self.circular_deps) <= self.built_packages:
+        print((set(self.graph.successors(package)) - self.circular_deps) <= self.built_packages)
+        if (set(self.graph.successors(package)) - self.circular_deps) <= self.built_packages:
             return True         # TODO resolve circular deps
         return False
 
@@ -101,19 +132,6 @@ def update_dict(dictionary, key, value):
         dictionary[key].append(value)
     else:
         dictionary[key] = [value]
-
-def get_deps(package, repo):
-    '''
-    Returns all dependancies of the package found in selected repo
-    '''
-    proc = Popen(["dnf", "repoquery", "--arch=src", "--disablerepo=*", 
-        "--enablerepo=" + repo, "--requires", package], stdout=PIPE, stderr=PIPE)
-    stream_data = proc.communicate()
-    if proc.returncode:
-        if stream_data[1].decode(locale.getpreferredencoding()) ==\
-                "Error: Unknown repo: '{0}'\n".format(repo):
-                    raise UnknownRepoException('Repository {} is probably disabled'.format(repo))
-    return stream_data[0].decode(locale.getpreferredencoding()).splitlines()[1:]
 
 def base_name(name):
     '''

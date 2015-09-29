65d5ad7f327f3fb9c73ad70f7b613046be3d68f6
Michal Cyprian <mcyprian@redhat.com>
2015-09-29 19:08:59 +0200

    Add functionality to srpm_archive - Add get_file to find specific files in tempfile - Improve unpack method - Add change_dir class to utils

--- sclbuilder/srpm_archive.py
+++ sclbuilder/srpm_archive.py
@@ -3,20 +3,32 @@ import glob
 from subprocess import Popen, PIPE, CalledProcessError
 
 import sclbuilder.exceptions as ex
-from sclbuilder.utils import subprocess_popen_call
+from sclbuilder import settings
+from sclbuilder.utils import subprocess_popen_call, change_dir
 
 
 class SrpmArchive(object):
     '''
     Contains methods to work with srpm archive.
     '''
-    def __init__(self, temp_dir, package, repo, srpm_name=''):
-        self.temp_dir = temp_dir         #TODO check if ends with '/'
+    def __init__(self, temp_dir, package, repo=settings.DEFAULT_REPO, srpm_name=None):
+        self.temp_dir = temp_dir
         self.package = package
         self.repo = repo
         self.srpm_name = srpm_name
 
-    def download_srpm(self):
+    @property
+    def temp_dir(self):
+        return self._temp_dir
+
+    @temp_dir.setter
+    def temp_dir(self, path):
+        if path[-1] == '/':
+            self._temp_dir = path
+        else:
+            self._temp_dir = path + '/'
+
+    def download(self):
         '''
         Download srpm of package from selected repo.
         '''
@@ -29,22 +41,35 @@ class SrpmArchive(object):
                     raise ex.UnknownRepoException('Repository {} is probably disabled'.format(self.repo))
         elif proc_data['stderr']:
             raise ex.DownloadFailException(proc_data['stderr'])
-        
-        srpm_name = glob.glob(self.temp_dir + self.package + '*.src.rpm')
-        if not srpm_name:
-            raise ex.SrpmNotFoundException("Failed to find srpm of package {}".format(
-                self.package))
-        else:
-            self.srpm_name = srpm_name[0][len(self.temp_dir):]
 
-    def unpack_srpm(self):        #TODO cd temp ... 
+        self.srpm_name = self.get_file('.src.rpm')
+        print(self.srpm_name)
+        
+        
+    def unpack(self):
         '''
         Unpacks srpm archive
         '''
-        p1 = Popen(["rpm2cpio", self.temp_dir + self.srpm_name], stdout=PIPE,
-                stderr=PIPE)
-        p2 = Popen(["cpio", "-idmv"], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
-        stream_data = p2.communicate()
-        stderr_str = stream_data[1].decode(locale.getpreferredencoding())
-        if p2.returncode:
-            raise CalledProcessError(cmd='rpm2cpio' ,returncode=p2.returncode)
+        with change_dir(self.temp_dir):
+            p1 = Popen(["rpm2cpio", self.temp_dir + self.srpm_name], stdout=PIPE,
+                    stderr=PIPE)
+            p2 = Popen(["cpio", "-idmv"], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
+            stream_data = p2.communicate()
+            stderr_str = stream_data[1].decode(locale.getpreferredencoding())
+            if p2.returncode:
+                raise CalledProcessError(cmd='rpm2cpio' ,returncode=p2.returncode)
+            self.spec_file = self.get_file('.spec')
+            print(self.spec_file)
+
+    def get_file(self, suffix):
+        '''
+        Checks if file self.package.suffix exists in self.temp_dir
+        returns file name on success
+        '''
+        name = glob.glob(self.temp_dir + '*' + suffix)
+        if not name:
+            raise FileNotFoundError("Failed to find {}".format(self.package
+                + '*' + suffix))
+        else:
+            return name[0][len(self.temp_dir):]
+
--- sclbuilder/utils.py
+++ sclbuilder/utils.py
@@ -1,6 +1,19 @@
 import locale
+import os
 
 from subprocess import Popen, PIPE
+
+def add_prefix(name, prefix):   #TODO remove prefix functions??
+    if prefix in name:
+        return name
+    else:
+        return prefix + name
+
+def remove_prefix(name, prefix):
+    if not prefix in name:
+        return name
+    else:
+        return name[len(prefix):]
 
 def subprocess_popen_call(command=[]):
     proc = Popen(command, stdout=PIPE, stderr=PIPE)
@@ -8,3 +21,19 @@ def subprocess_popen_call(command=[]):
     stdout_str = stream_data[0].decode(locale.getpreferredencoding())
     stderr_str = stream_data[1].decode(locale.getpreferredencoding())
     return {'returncode' : proc.returncode, 'stdout' : stdout_str, 'stderr' : stderr_str}
+
+class change_dir(object):
+    '''
+    With statement class to store current dir change it and return
+    to previous path.
+    '''
+    def __init__(self, new_path):
+        self.primary_path = os.getcwd()
+        self.new_path = new_path
+
+    def __enter__(self):
+        os.chdir(self.new_path)
+        return self
+
+    def __exit__(self, type, value, traceback): #TODO handle exception
+        os.chdir(self.primary_path)
